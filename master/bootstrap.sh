1bc34d28fd6354b889d5dad1ad2d57426fe69201
asuchocki <anthony.suchocki@gmail.com>
2016-04-26 19:47:10 -0700

    Implement automated r10k setup

--- master/bootstrap.sh
+++ master/bootstrap.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+rm /etc/localtime
+ln -s /usr/share/zoneinfo/America/Phoenix /etc/localtime
 rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-yum install -y puppetserver
+yum install -y puppetserver git
 sed -i -- 's/JAVA_ARGS="-Xms2g -Xmx2g -XX:MaxPermSize=256m"/JAVA_ARGS="-Xms512m -Xmx512m -XX:MaxPermSize=256m"/g' /etc/sysconfig/puppetserver
 systemctl enable puppetserver
 systemctl start puppetserver
-/opt/puppetlabs/bin/puppet module install puppetlabs-ntp
+/opt/puppetlabs/puppet/bin/gem install r10k
+cp -r /home/vagrant/sync/master/r10k /etc/puppetlabs
+/opt/puppetlabs/puppet/bin/r10k deploy environment
